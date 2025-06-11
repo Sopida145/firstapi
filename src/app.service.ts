6e0f27d85f946a6a2063a7f1932782456d2797ee
pond <pond.sopida@gmail.com>
2025-06-11 23:38:58 +0700

    ok

---
+++ src/app.service.ts
@@ -0,0 +1,13 @@
+import { Injectable } from '@nestjs/common';
+
+@Injectable()
+export class AppService {
+  getHello(): any {
+    return {
+      message: 'Hello World!',
+      timestamp: new Date().toISOString(),
+      status: 'success',
+  
+    };
+  }
+}
